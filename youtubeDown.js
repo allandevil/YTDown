885e3e7d53b21d400c28500a1fc52c6603285504
allandevil <allanvicente58@gmail.com>
2017-08-16 15:20:16 -0300

    Começando Server DownYT

--- youtubeDown.js
+++ youtubeDown.js
@@ -1,5 +1,40 @@
 var fs = require('fs');
+var express = require('express'), app = express();
 var youtubedl = require('youtube-dl');
+var bodyParser = require('body-parser');
+
+// Callback function
+var mycallback = function(err,results) {
+	console.log("mycallback");
+	if(err) throw err;
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res){
+ 	Balada.find(function(err, result){
+        res.send(result);
+     });
+});
+
+ app.post('/balada', function(req, res){
+ 	if(req.body.nome){
+ 		Balada.find({ nome: req.body.nome}, function(err, result){
+ 			res.send(result);
+ 		});
+ 	}
+ });
+
+ app.post('/',function(req,res){
+   var novo = new Balada(req.body);
+ 	novo.save(function(err){
+ 		if(err) throw err;
+ 		res.send("Dados Salvos com sucesso!")
+ 	});
+ });
+ app.listen(1337);
+
 var video = youtubedl('https://www.youtube.com/watch?v=k34CRoPRM8A',
   // Optional arguments passed to youtube-dl.
   ['--format=18'],
