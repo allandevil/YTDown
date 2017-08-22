45e7644a55e7b2ec9b7974442b22a3509ea362d9
allandevil <allanvicente58@gmail.com>
2017-08-22 14:52:13 -0300

    Atualização servico rest e teste com celular

--- youtubeDown.js
+++ youtubeDown.js
@@ -13,26 +13,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
- 	console.log("Inda não implementado");
+ 	console.log("Entrou no get");
+	var novo = req.body;
+	console.log(novo,cont);
+	return res.send("Dados Salvos com sucesso!");
 });
 
  app.post('/',function(req,res){
-   var novo = new Balada(req.body);
- 	 console.log(novo);
+	 console.log("Entrou no post");
+   var novo = req.body;
+ 	 console.log(novo,cont);
+	 return res.send("Dados Salvos com sucesso!");
  });
+
  app.listen(1337);
 
-var video = youtubedl('https://www.youtube.com/watch?v=k34CRoPRM8A',
-  // Optional arguments passed to youtube-dl.
-  ['--format=18'],
-  // Additional options can be given for calling `child_process.execFile()`.
-  { cwd: __dirname });
+
+var video = youtubedl(url,
+	// Optional arguments passed to youtube-dl.
+	['--format=18'],
+	// Additional options can be given for calling `child_process.execFile()`.
+	{ cwd: __dirname });
 
 // Will be called when the download starts.
 video.on('info', function(info) {
-  console.log('Download started');
-  console.log('filename: ' + info.filename);
-  console.log('size: ' + info.size);
+ console.log('Download started');
+ console.log('filename: ' + info.filename);
+ console.log('size: ' + info.size);
 });
 
 video.pipe(fs.createWriteStream('../myvideo.mp4'));
